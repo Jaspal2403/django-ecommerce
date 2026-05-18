dd0dd13ee2dc735af86f93837b7592b1d74cb968
Jaspal <test@gmail.com>
2026-05-18 16:01:47 +0530

    SMTP function added for forgot password

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -233,7 +233,21 @@ RAZORPAY_WEBHOOK_SECRET = "preetcraft_webhook_secret_2026"
 # EMAIL CONFIGURATION
 # =========================================
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'jaspaltsingh@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'acah dpxa aran wdyd'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # =========================================
 # RAZORPAY CREDENTIALS
